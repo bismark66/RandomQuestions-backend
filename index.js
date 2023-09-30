83b55c9c7e8dde21597c6f83149ea795a0e454e7
Bismark <agyarebismark6@gmail.com>
2023-09-30 21:39:18 +0000

    edited app listen response

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(router());
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
