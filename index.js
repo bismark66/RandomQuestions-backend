ecea8f9295960008f123b7b95a839a2636acdd3a
Bismark <agyarebismark6@gmail.com>
2023-09-30 21:33:13 +0000

    made few changes

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router());
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
